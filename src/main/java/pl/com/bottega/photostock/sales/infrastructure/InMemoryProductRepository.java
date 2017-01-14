7e04fdc97fe032fc03b4ca143f81593996d0d5be
Maciej Zubala <maciej.zubala@gmail.com>
2017-01-14 15:38:44 +0100

    Search only availabe products

--- src/main/java/pl/com/bottega/photostock/sales/infrastructure/InMemoryProductRepository.java
+++ src/main/java/pl/com/bottega/photostock/sales/infrastructure/InMemoryProductRepository.java
@@ -38,10 +38,10 @@ public class InMemoryProductRepository implements ProductRepository {
     }
 
     @Override
-    public List<Product> find(Client client, String nameQuery, String[] tags, Money priceFrom, Money priceTo, boolean onlyActive) {
+    public List<Product> find(Client client, String nameQuery, String[] tags, Money priceFrom, Money priceTo, boolean onlyAvailable) {
         List<Product> matchingProducts = new LinkedList<>();
         for (Product product : REPOSITORY.values()) {
-            if (matches(client, product, nameQuery, tags, priceFrom, priceTo, onlyActive))
+            if (matches(client, product, nameQuery, tags, priceFrom, priceTo, onlyAvailable))
                 matchingProducts.add(product);
         }
         return matchingProducts;
@@ -51,7 +51,12 @@ public class InMemoryProductRepository implements ProductRepository {
         return matchesQuery(product, nameQuery) &&
                 matchesTags(product, tags) &&
                 matchesPriceFrom(client, product, priceFrom) &&
-                matchesPriceTo(client, product, priceTo);
+                matchesPriceTo(client, product, priceTo) &&
+                matchesOnlyActive(product, onlyActive);
+    }
+
+    private boolean matchesOnlyActive(Product product, boolean onlyAvailable) {
+        return !onlyAvailable || product.isAvailable();
     }
 
     private boolean matchesPriceTo(Client client, Product product, Money priceTo) {
